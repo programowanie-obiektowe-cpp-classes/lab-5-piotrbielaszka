6ecce176a98eb9a8564da8e7217982bc6d6ece34
Piotr Bielaszka <piotr.bielaszka@gmail.com>
2024-11-19 13:56:28 +0100

    skonczone

--- foo.hpp
+++ foo.hpp
@@ -8,5 +8,15 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
-    return {};
+    std::vector< char > ret;
+    for (Human& h : people) {
+        h.birthday();
+        if (h.isMonster())
+            ret.push_back('n');
+        else
+            ret.push_back('y');
+    }
+    std::reverse(ret.begin(), ret.end());
+
+    return ret;
 }
